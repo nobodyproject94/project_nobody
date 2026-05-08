99b5b8bdcdc79b99f8913f82918ac40ecc7d31a4
nobodyproject94 <nobodyproject94@gmail.com>
2026-05-08 09:06:15 +0700

    tugas1flutter

--- lib/day_1/tugas1flutter.dart
+++ lib/day_1/tugas1flutter.dart
@@ -6,9 +6,39 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 248, 248, 248),
-      appBar: AppBar(title: Text('profile')),
-      theme: 
+      backgroundColor: const Color.fromARGB(255, 207, 12, 12),
+      appBar: AppBar(title: Text('My Profile'), centerTitle: true),
+      body: Column(
+        children: [
+          const Text(
+            "nama: nobody",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.location_pin, size: 20),
+              Text(
+                "heaven",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "from nothing to something",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
